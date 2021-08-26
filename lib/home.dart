a5388c945beb0f67f7c86c3d62f2827289ad5300
santaat <s6302041520171@email.kmutnb.ac.th>
2021-08-26 17:52:15 +0700

    work same after week

---
+++ lib/home.dart
@@ -0,0 +1,88 @@
+import 'package:flutter/material.dart';
+
+class HOME extends StatelessWidget {
+  const HOME({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    var size = MediaQuery.of(context).size;
+    return Scaffold(
+      
+      body: Container(
+        alignment: Alignment.center,
+        child: Stack(
+          
+          children: <Widget>[
+            Positioned(
+              top: -80,
+              left: -90,
+                child: Image.asset(
+                "asset/img/orange-circle.png",
+                width: size.width*0.5,
+              ),
+            ),
+            Column(
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: [
+                
+                SizedBox( height: 20,),
+                Image.asset(
+                  "asset/img/logo_kmutnb.png",
+                  width: size.width*0.5,
+                  height: size.height*0.3,
+                  ),
+                
+                Text(
+                  "Welcome to KMUTNB.",
+                    style: TextStyle(
+                    fontSize: 35,
+                    color: Colors.redAccent,
+                  ),
+                ),
+
+                Image.asset(
+                  "asset/img/undraw_our_neighborhood_ya1h.png",
+                  width: size.width,
+                  height: size.height*0.3,
+                  ),
+
+                SizedBox( height: 50,
+                ),
+
+                ElevatedButton(
+                  style: ElevatedButton.styleFrom(
+                    textStyle: TextStyle(
+                      fontSize: 25, 
+                    ),
+                    primary: Colors.red[200],
+                    padding:  EdgeInsets.fromLTRB(120, 20, 120, 20),
+                    
+                    shape: StadiumBorder(),
+                  ),child: const Text("Login"),
+                  onPressed:(){ print("Click me");}
+                ),
+
+                SizedBox( height: 25,
+                ),
+                
+                ElevatedButton(
+                  style: ElevatedButton.styleFrom(
+                    
+                    textStyle: TextStyle(
+                      fontSize: 25, 
+                    ),
+                    primary: Colors.redAccent,
+                    padding:  EdgeInsets.fromLTRB(110, 20, 110, 20),
+                    shape: StadiumBorder(),
+                  ),child: const Text("SIGN UP"),
+                  onPressed:(){ print("Click me");}
+                ),
+                
+              ],
+            ),
+          ],
+        )
+      ),
+    );
+  }
+}
---
+++ lib/test.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+class Test extends StatelessWidget {
+  const Test({ Key? key }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        child:Stack(
+          children: [
+             Image.asset(
+                  "asset/img/orange-circle.png",
+                  width: 100,
+                  height: 100,
+                  ),
+                
+          ],
+        ),
+      ),
+    );
+  }
+}
